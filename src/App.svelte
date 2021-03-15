f144c476947ae9f4b3354d16a9f84c2ada06de58
fmoor <fmoor@gmx.com>
2021-03-15 14:55:14 -0700

    add routing with page.js

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,25 @@
 <script>
-	export let name;
+  import router from "page"
+  import routes from './routes'
+
+	let params;
+	let page;
+
+  routes.forEach(route => {
+    router(
+      route.path,
+      (ctx, next) => {
+        params = ctx.params
+				page = route.component
+      }
+    )
+  })
+
+	router.start()
 </script>
 
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+ <svelte:component this={page} params={params} />
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
