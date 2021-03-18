cb105bb84ae5dd4900dc084d9056953f7e6d19ff
fmoor <fmoor@gmx.com>
2021-03-18 15:45:06 -0700

    remove duplicate main tags

--- src/pages/Index.svelte
+++ src/pages/Index.svelte
@@ -1,7 +1,5 @@
-<main>
-	<h1>Hello World!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<h1>Hello World!</h1>
+<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 
 <style>
 	main {
